3a11e5b136b5c3abb8cece2f065c49a1208725c9
李欢欢 <lhh@lihuanhuandeMac-mini.local>
2024-11-09 19:15:20 +0800

    配置gitignore

---
+++ Server/ReadMe.md
@@ -0,0 +1,18 @@
+# Readme
+
+## 1. 本项目是Nodejs服务后端
+
+技术栈
+
+1. Express服务框架
+2. moggonDB
+
+项目启动
+
+```shell
+npm run start
+brew services start mongodb-community@8.0 // 启动
+
+brew services stop mongodb-community@8.0 // 停止
+```
+
--- Server/src/db.js
+++ Server/src/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://localhost:27017/mydatabase');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
